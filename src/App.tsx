b983cee3e29527025035aa45f0c46a0aa2351056
marco-soria <marco.soriaa7@gmail.com>
2024-06-28 16:33:41 -0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+
+
+
+function App() {
+  
+
+  return (
+    <>
+      <div className="bg-home-car h-screen bg-center bg-cover ">
+        <p className='text-6xl'>
+          Hello angel
+        </p>
+      </div>
+      
+    </>
+  )
+}
+
+export default App
