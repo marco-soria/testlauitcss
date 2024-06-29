7542cd1ebc5b23b0be3b7e7a6a2dcb358446e221
marco-soria <marco.soriaa7@gmail.com>
2024-06-28 22:15:01 -0500

    footer completed

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,8 +2,44 @@
 
 export const Footer = () => {
   return (
-    <div className="bg-black">
-      <p className="text-white"> Footer</p>
+    <div className="bg-black pt-10">
+      <div className="flex flex-col justify-center items-center text-white">
+          <p className="whitespace-no-wrap pt-1 text-[12px]"><sup>1</sup>&nbsp;Price before estimated savings is $47,490, excluding taxes and fees. Subject to change.</p>
+          <p className="underline underline-offset-2 font-bold text-[12px]">
+          Learn about est. gas savings.
+          </p>
+          
+          <p className="whitespace-no-wrap pt-5 text-[12px]"><sup>2</sup>&nbsp;Price before estimated savings is $44,490, excluding taxes and fees. Subject to change.</p>
+          
+          <p className="underline underline-offset-2 font-bold text-[12px]">
+          Learn about est. gas savings.
+          </p>
+          
+          <p className="whitespace-no-wrap pt-5 text-[12px]"><sup>3</sup>&nbsp;Price before estimated savings is $47,490, excluding taxes and fees. Subject to change.</p>
+          
+          <p className="underline underline-offset-2 font-bold text-[12px]">
+          Learn about est. gas savings.
+          </p>
+          
+          <p className="whitespace-no-wrap pt-5 text-[12px]"><sup>4</sup>&nbsp;Price before estimated savings is $47,490, excluding taxes and fees. Subject to change.</p>
+          
+          <p className="underline underline-offset-2 font-bold text-[12px]">
+          Learn about est. gas savings.
+          </p>
+          
+        </div>
+      <footer className="flex pb-10 justify-center items-center mt-20">
+        
+       <ul className="flex flex-col justify-center items-center md:flex-row md:space-x-4  text-[12px] text-white">
+           <li className="link">Tesla &copy; 2024</li>
+           <li className="link">Privacy & Legal</li>
+           <li className="link">Vehicle Recalls</li>
+           <li className="link">Contact</li>
+           <li className="link">News</li>
+           <li className="link hidden md:inline">Get Updates</li>
+           <li className="link hidden md:inline">Location</li>
+       </ul>
+   </footer>
       </div>
   )
 }
